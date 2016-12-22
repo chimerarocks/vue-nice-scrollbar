893b82a96c64101eec300487b38e6189efa92d6d
João Pedro <joaopedrodslv@gmail.com>
2016-12-22 05:41:48 -0200

    change main to load only component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
+import vueNiceScrollbar from './components/vue-nice-scrollbar.vue'
 
-new Vue({
-    el: '#app',
-    render: h => h(App)
-})
+export default vueNiceScrollbar
